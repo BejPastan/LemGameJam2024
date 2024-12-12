e7d0ca486de15320034ebfdd008019b16e2d512b
PawelB2022 <bieniakonskipawel@gmail.com>
2024-12-12 21:42:21 +0100

    Dopełniono logikę przycisku podłogowego i dodano opcję one_shot

--- Assets/Scripts/PressurePlate.cs
+++ Assets/Scripts/PressurePlate.cs
@@ -2,13 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PressurePlate : MonoBehaviour
+public class PressurePlate : DefaultInteraction
 {
+    public override void Interact(Transform agent, bool callEvent = true)
+    {
+        base.Interact(agent, callEvent);
+    }
     public GameObject plateButton;
     private Transform plateButtonTransform;
     private MeshRenderer plateButtonRenderer;
 
+    [Header("Button States")]
     private bool is_triggereed = false;
+    public bool is_one_shot = false;
 
     [Header("Color Settings")]
     public Color triggeredColor = Color.red;  // Color to change to when triggered
@@ -33,17 +39,23 @@ public class PressurePlate : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Body entered the trigger zone!");
-        is_triggereed = true;
-        plateButtonTransform.Translate(new Vector3(0.0f, -0.08f, 0.0f));
-        plateButtonRenderer.material.color = triggeredColor;
+        if (other.CompareTag("Player"))
+        {
+            is_triggereed = true;
+            plateButtonTransform.Translate(new Vector3(0.0f, -0.08f, 0.0f));
+            plateButtonRenderer.material.color = triggeredColor;
+            Interact(transform);
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        Debug.Log("Body exited the trigger zone!");
-        is_triggereed = false;
-        plateButtonTransform.Translate(new Vector3(0.0f, 0.08f, 0.0f));
-        plateButtonRenderer.material.color = defaultColor;
+        if (other.CompareTag("Player") && is_one_shot == false)
+        {
+            is_triggereed = false;
+            plateButtonTransform.Translate(new Vector3(0.0f, 0.08f, 0.0f));
+            plateButtonRenderer.material.color = defaultColor;
+            Interact(transform);
+        }
     }
 }
