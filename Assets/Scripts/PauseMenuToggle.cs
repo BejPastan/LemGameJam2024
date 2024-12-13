e677b82c747a284786cada99ef009cfadc9bccde
BejPastan <s.synaszko@gmail.com>
2024-12-13 12:12:35 +0100

    Ups

--- Assets/Scripts/PauseMenuToggle.cs
+++ Assets/Scripts/PauseMenuToggle.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenuToggle : MonoBehaviour
 {
     static Transform pauseMenu;
     static bool isPaused = false;
+
+    private void Start()
+    {
+        pauseMenu = transform.Find("PauseMenu");
+    }
 
     public static void TogglePauseMenu()
     {
@@ -19,5 +25,20 @@ public class PauseMenuToggle : MonoBehaviour
             isPaused = true;
             pauseMenu.gameObject.SetActive(true);
         }
+    }
+
+    public static void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public static void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public static void StartGame()
+    {
+        SceneManager.LoadScene("Level1Testowy");
     }
 }
