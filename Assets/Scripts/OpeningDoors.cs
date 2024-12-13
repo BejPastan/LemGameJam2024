d137e99e4916b51d3bc72e6787ba672d3579666d
BejPastan <s.synaszko@gmail.com>
2024-12-13 15:07:26 +0100

    asdasd

--- Assets/Scripts/OpeningDoors.cs
+++ Assets/Scripts/OpeningDoors.cs
@@ -5,29 +5,37 @@ using UnityEngine;
 public class OpeningDoors : DefaultInteraction
 {
     public bool isOpen = false;
+    public bool isOpening = false;
     public float openAngle = 90.0f;
     public float openSpeed = 2.0f;
+
+    float angleChange = 0;
 
     public override void Interact(Transform transform, bool callEvent = true)
     {
         base.Interact(transform);
-        if (isOpen)
+        if (isOpen || isOpening)
         {
+            StopAllCoroutines();
             StartCoroutine(CloseDoor());
         }
         else
         {
+            StopAllCoroutines();
             StartCoroutine(OpenDoor());
         }
     }
 
     IEnumerator OpenDoor()
     {
+        isOpening = true;
         float currentAngle = transform.localEulerAngles.y;
-        float targetAngle = currentAngle + openAngle;
+        float targetAngle = openAngle;
+        Debug.Log("Target open angle: " + targetAngle);
         while (currentAngle < targetAngle)
         {
             currentAngle += openSpeed * Time.deltaTime;
+            angleChange += openSpeed * Time.deltaTime;
             transform.Rotate(0, openSpeed * Time.deltaTime, 0);
             yield return null;
         }
@@ -36,11 +44,17 @@ public class OpeningDoors : DefaultInteraction
 
     IEnumerator CloseDoor()
     {
+        isOpening = false;
         float currentAngle = transform.localEulerAngles.y;
-        float targetAngle = currentAngle - openAngle;
+        Debug.Log("Current angle: " + currentAngle);
+        Debug.Log("Angle change: " + angleChange);
+        float targetAngle = currentAngle - angleChange;
+        
+        Debug.Log("Target close angle: " + targetAngle);
         while (currentAngle > targetAngle)
         {
             currentAngle -= openSpeed*Time.deltaTime;
+            angleChange -= openSpeed * Time.deltaTime;
             transform.Rotate(0, -openSpeed * Time.deltaTime, 0);
             yield return null;
         }
